9b3d3b17c6c4dc220e30bbdad9d573980edf2f3f
Patrick Farnworth <86934924+FarnworthDesigns@users.noreply.github.com>
2022-05-09 16:50:42 -0700

    Add files via upload

---
+++ project.000686717/README.md
@@ -0,0 +1,10 @@
+# My Topic Google Doodle
+
+My topic for Google Doodle is "International Astronomy Day". International Astronomy Day is a way for astronomy enthusiasts and professionals to share their knowledge and love of outer space with the general public. It is also a way for everyone interested in space to explore their passion and increase their knowledge.
+
+## Collage
+
+[Sample to Figma Collage](https://www.figma.com/file/1OV4qHG1edd6Ntw4YsDrJp/Collage?node-id=0%3A1)
+
+## Resources
+[Resources](https://www.daysoftheyear.com/days/astronomy-day/)
---
+++ project.000686717/js/scripts.js
@@ -0,0 +1,110 @@
+// GSAP included in `index.html`
+console.log('OK');
+var star13 = gsap.timeline({repeat: -1, repeatDelay: .5, yoyo: true});
+star13.to('#Star_13', {
+  transformOrigin:"50% 50%",
+  rotation: 20,
+  duration:2
+});
+var star12 = gsap.timeline({repeat: -1, repeatDelay: .5, yoyo: true});
+star12.to('#Star_12', {
+  transformOrigin:"50% 50%",
+  rotation: 36,
+  duration:4
+});
+var star11 = gsap.timeline({repeat: 1, repeatDelay: .5, yoyo: true});
+star11.to('#Star_11', {
+  transformOrigin:"50% 50%",
+  rotation: 20,
+  duration:3
+});
+var star10 = gsap.timeline({repeat: -1, repeatDelay: .5, yoyo: true});
+star10.to('#Star_10', {
+  transformOrigin:"50% 50%",
+  rotation: 20,
+  duration:2
+});
+var star9 = gsap.timeline({repeat: -1, repeatDelay: .5, yoyo: true});
+star9.to('#Star_9', {
+  transformOrigin:"50% 50%",
+  rotation: 127,
+  duration:2
+});
+var star8 = gsap.timeline({repeat: -1, repeatDelay: .5, yoyo: true});
+star8.to('#Star_8', {
+  transformOrigin:"50% 50%",
+  rotation: 90,
+  duration:2
+});
+var star7 = gsap.timeline({repeat: -1, repeatDelay: .5, yoyo: true});
+star7.to('#Star_7', {
+  transformOrigin:"50% 50%",
+  rotation: 13,
+  duration:2
+});
+var star6 = gsap.timeline({repeat: -1, repeatDelay: .5, yoyo: true});
+star6.to('#Star_6', {
+  transformOrigin:"50% 50%",
+  rotation: 45,
+  duration:2
+});
+var star5 = gsap.timeline({repeat: -1, repeatDelay: .5, yoyo: true});
+star5.to('#Star_5', {
+  transformOrigin:"50% 50%",
+  rotation: 89,
+  duration:2
+});
+var star4 = gsap.timeline({repeat: -1, repeatDelay: .5, yoyo: true});
+star4.to('#Star_4', {
+  transformOrigin:"50% 50%",
+  rotation: 52,
+  duration:2
+});
+var star3 = gsap.timeline({repeat: -1, repeatDelay: .5, yoyo: true});
+star3.to('#Star_3', {
+  transformOrigin:"50% 50%",
+  rotation: 35,
+  duration:2
+});
+var star2 = gsap.timeline({repeat: -1, repeatDelay: .5, yoyo: true});
+star2.to('#Star_2', {
+  transformOrigin:"50% 50%",
+  rotation: 40,
+  duration:2
+});
+var star1 = gsap.timeline({repeat: -1, repeatDelay: .5, yoyo: true});
+star1.to('#Star_1', {
+  transformOrigin:"50% 50%",
+  rotation: 20,
+  duration:2
+});
+var red = gsap.timeline({repeat: -1, repeatDelay: .5, yoyo: true});
+red.to('#Red_Planet', {
+  rotation:10,
+  duration:10
+});
+var blue = gsap.timeline({repeat: -1, repeatDelay: .5, yoyo: true});
+blue.to('#Blue_Planet', {
+  rotation:-10,
+  duration:2
+});
+var yellow = gsap.timeline({repeat: -1, repeatDelay: .5, yoyo: true});
+yellow.to('#Yellow_Planet', {
+  rotation:-35,
+  duration: 4
+});
+var leaf4 = gsap.timeline({repeat: -1, repeatDelay: .5, yoyo: true});
+leaf4.to('#Leaf_4', {
+  rotation: 5,
+  duration:4
+});
+var leaf1 = gsap.timeline({repeat: -1, repeatDelay: .5, yoyo: true});
+leaf1.to('#Leaf_1', {
+  rotation: 5,
+  duration:4
+});
+var tele = gsap.timeline({repeat:-1, repeatDelay: .5, yoyo: true});
+tele.to('#Telescope_Top', {
+  rotation:-4,
+  duration:10
+});
